eae34bd2304c9d4b021eb36907d06d78aae683ae
didacdedm <didacdedm@hotmail.com>
2017-06-29 19:56:36 -0500

    Fin de la Clase

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = ()=>(
+  // React.createElement('div', {className:'text-center'}, 'Hola Mundo')
+  <h1>Clase de React 1</h1>
+);
+
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -7,3 +7,22 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// const MiPrimerComponente = ()=>(
+//   // React.createElement('div', {className:'text-center'}, 'Hola Mundo')
+//   <div className="text-center">
+//     Hola Mundo
+//   </div>
+// );
+
+
+
+
+// ReactDOM.render(
+//   // React.createElement(miPrimerComponente, null, null),document.getElementById('root')
+//   <MiPrimerComponente />,document.getElementById('root')
+// );
