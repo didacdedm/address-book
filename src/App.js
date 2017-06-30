13177f76c064746a7ccb189e043e3f11c69efe43
didacdedm <didacdedm@hotmail.com>
2017-06-29 20:05:39 -0500

    Final final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/Header.js'
+import Footer from './components/Footer.js'
 
 class App extends Component {
   render() {
     return (
       
-      <div className="text-center"><Header /> Hola Mundo</div>
+      <div className="text-center"><Header title="Address Book"/> Hola Mundo <Footer copyrigth="2017 Diego Del Salto"/> </div>
     );
   }
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = (props)=>(
+const Footer = (props)=>(
   // React.createElement('div', {className:'text-center'}, 'Hola Mundo')
-  <h1>{props.title}</h1>
+  <small>{props.copyrigth}</small>
 );
 
 
-export default Header;
+export default Footer;
