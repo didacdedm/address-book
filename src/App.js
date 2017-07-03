7c320ffcd291c0cb4e10fa121b3d36e5bd8cbf20
didacdedm <didacdedm@hotmail.com>
2017-07-03 10:27:14 -0500

    Aaa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
+import SearchBox from './components/SearchBox.js'
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      searchText:'Buscar',
+    };
+  }
+
+  handleSearchTextChange=(event)=>{
+    this.setState({
+      searchText: event.target.value
+    })
+  }
+
   render() {
     return (
       
-      <div className="text-center"><Header title="Address Book"/> Hola Mundo <Footer copyrigth="2017 Diego Del Salto"/> </div>
+      <div className="text-center"><Header title="Address Book"/> 
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6"><SearchBox value={this.state.searchText} onChange={this.handleSearchTextChange}/></div>
+          <div className="col-md-6">Nuevo Contacto</div>
+        </div>
+      </div> 
+      <div><Footer copyrigth="2017 Diego Del Salto"/></div>
+      </div>
     );
   }
 }
